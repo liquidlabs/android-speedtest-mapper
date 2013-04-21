56c3b25513941cb9f0b46674b63f4ee8219e2af5
Hossain Khan <amardeshbd@gmail.com>
2013-04-21 05:29:42 -0400

    Updated connection type - added useful method that can be used for UI.

--- src/ca/liquidlabs/android/speedtestmapper/util/Constants.java
+++ src/ca/liquidlabs/android/speedtestmapper/util/Constants.java
@@ -3,4 +3,5 @@ package ca.liquidlabs.android.speedtestmapper.util;
 public interface Constants {
     boolean DEBUG_MODE = true;
     String TAG_TRACE = "[STM]";
+    String SPEEDTEST_PACKAGE = "org.zwanoo.android.speedtest";
 }
