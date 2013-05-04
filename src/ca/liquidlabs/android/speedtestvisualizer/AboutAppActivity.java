1519ab5d2e4fa01b2e9fc9780ab9788767055c36
Hossain Khan <amardeshbd@gmail.com>
2013-05-04 19:00:33 -0400

    Renamed package to reflect app name.

--- src/ca/liquidlabs/android/speedtestvisualizer/AboutAppActivity.java
+++ src/ca/liquidlabs/android/speedtestvisualizer/AboutAppActivity.java
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ca.liquidlabs.android.speedtestmapper;
+package ca.liquidlabs.android.speedtestvisualizer;
 
+import ca.liquidlabs.android.speedtestvisualizer.R;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import android.app.Activity;
--- src/ca/liquidlabs/android/speedtestvisualizer/util/Tracer.java
+++ src/ca/liquidlabs/android/speedtestvisualizer/util/Tracer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ca.liquidlabs.android.speedtestmapper.util;
+package ca.liquidlabs.android.speedtestvisualizer.util;
 
 import android.app.Activity;
 import android.content.Context;
