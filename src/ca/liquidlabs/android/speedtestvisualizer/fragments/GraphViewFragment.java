fbda3f05b38246d36a1c62c4cbb70be526c9e331
Hossain Khan <amardeshbd@gmail.com>
2013-08-18 20:30:19 -0400

    Sample graph view fragment from the GraphView project.

--- src/ca/liquidlabs/android/speedtestvisualizer/fragments/GraphViewFragment.java
+++ src/ca/liquidlabs/android/speedtestvisualizer/fragments/GraphViewFragment.java
@@ -1,5 +1,5 @@
-package ca.liquidlabs.android.speedtestvisualizer.fragments;
 
+package ca.liquidlabs.android.speedtestvisualizer.fragments;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,51 +10,50 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import ca.liquidlabs.android.speedtestvisualizer.R;
+import ca.liquidlabs.android.speedtestvisualizer.util.SpeedTestRecordProcessorTask.OnDataProcessorListener;
 
 import com.jjoe64.graphview.BarGraphView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewDataInterface;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
 
-public class GraphViewFragment  extends Fragment {
+public class GraphViewFragment extends BaseGraphFragment {
     /**
-     * The fragment argument representing the section number for this
-     * fragment.
+     * The fragment argument representing the section number for this fragment.
      */
     public static final String ARG_SECTION_NUMBER = "section_number";
-    
+
     private LinearLayout graph1;
     private LinearLayout graph2;
-    
-    
-    public static Fragment newInstance(String content){
+
+    public static Fragment newInstance(String content) {
         GraphViewFragment fragment = new GraphViewFragment();
         Bundle bundle = new Bundle();
         bundle.putString(ARG_SECTION_NUMBER, content);
-        
+
         return fragment;
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_graphs, container, false);
-        graph1 =(LinearLayout) rootView.findViewById(R.id.graph1);
+        graph1 = (LinearLayout) rootView.findViewById(R.id.graph1);
         graph2 = (LinearLayout) rootView.findViewById(R.id.graph2);
- 
+
         return rootView;
     }
 
-
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
      * @see android.app.Fragment#onActivityCreated(android.os.Bundle)
      */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        
+
         // init example series data
         GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
                 new GraphViewData(1, 2.0d)
@@ -87,21 +86,36 @@ public class GraphViewFragment  extends Fragment {
             graphView = new BarGraphView(
                     getActivity().getApplicationContext()
                     , "GraphViewDemo"
-            );
+                    );
         } else {
             graphView = new LineGraphView(
                     getActivity().getApplicationContext()
                     , "GraphViewDemo"
-            );
+                    );
             ((LineGraphView) graphView).setDrawBackground(true);
             ((LineGraphView) graphView).setBackgroundColor(Color.rgb(80, 30, 30));
         }
         // custom static labels
-        graphView.setHorizontalLabels(new String[] {"2 days ago", "yesterday", "today", "tomorrow"});
-        graphView.setVerticalLabels(new String[] {"high", "middle", "low"});
+        graphView.setHorizontalLabels(new String[] {
+                "2 days ago", "yesterday", "today", "tomorrow"
+        });
+        graphView.setVerticalLabels(new String[] {
+                "high", "middle", "low"
+        });
         graphView.addSeries(exampleSeries); // data
 
         graph2.addView(graphView);
     }
-    
+
+    /**
+     * {@inheritDoc} <br/>
+     * 
+     * @see OnDataProcessorListener
+     */
+    @Override
+    public void onComplete(GraphViewDataInterface[] data) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
